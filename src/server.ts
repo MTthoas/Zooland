047079c016b534da9a6ac62135a6e236c6f59fd1
MTthoas <92540288+MTthoas@users.noreply.github.com>
2023-05-07 02:48:58 +0200

    Initialisation projet avec les exceptions de bases

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, {Express, Request, Response} from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use(express.json())
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+  });
+
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
