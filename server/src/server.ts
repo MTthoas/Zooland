224206044a18f55efca300de553460d20ac6f342
MTthoas <mpecquery@myges.fr>
2023-07-19 22:05:49 +0200

    push

--- server/src/server.ts
+++ server/src/server.ts
@@ -23,7 +23,7 @@ app.use(cors({
 }));
 
 
-app.use(function(req, res, next) {
+app.use(function(req : any, res: any, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
