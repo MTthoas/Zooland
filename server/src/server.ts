c8a118509851f081480fbaef80e1c546d89368b2
MTthoas <mpecquery@myges.fr>
2023-07-19 21:41:18 +0200

    modification headers

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,10 +20,11 @@ app.options('*', cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
+
 
 
 app.use(express.json());
