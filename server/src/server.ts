c0040048176aaf5bb02a56e04f9549815ec115c4
Matthias <92540288+MTthoas@users.noreply.github.com>
2023-07-19 16:10:26 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,6 +16,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors()) 
+app.use((err : any, req: any, res: any, next: any) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
