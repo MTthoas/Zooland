9d0ca6cc385b1e08a122fa461810b5034fde1c51
Matthias <92540288+MTthoas@users.noreply.github.com>
2023-07-19 16:19:36 +0200

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,17 +15,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 8080;
 
-const corsOptions = {
-  origin: 'http://localhost:3000', // ou vous pouvez mettre '*' pour autoriser toutes les origines
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
